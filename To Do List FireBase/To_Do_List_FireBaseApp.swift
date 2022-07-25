9307d125cfc8bcbb1a30d03e75a9a48e78a93fb6
Steve Lai <lzrong0203@gmail.com>
2022-07-25 22:24:55 +0800

    Initial Commit

---
+++ To Do List FireBase/To_Do_List_FireBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_FireBaseApp.swift
+//  To Do List FireBase
+//
+//  Created by Zhao-Rong Lai on 2022/7/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_FireBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
